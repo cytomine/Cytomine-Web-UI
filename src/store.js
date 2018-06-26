0d0100108f1f34a49aa9f41734ea760dc54e2043
eburtin <elodie.burtin@cytomine.coop>
2018-06-26 16:21:33 +0200

    Rework store with async/await syntax

--- src/store.js
+++ src/store.js
@@ -24,73 +24,37 @@ const store = new Vuex.Store({
 	},
 	actions: {
 
-		fetchUser({commit}) {
-			return new Promise(async (resolve, reject) => {
-				try {
-					let {data} = await axios.get("/server/ping.json");
-					commit('setAuthenticated', data.authenticated);
-					if(data.authenticated) {
-						({data} = await axios.get("/api/user/current.json"));
-						commit('setUser', data);
-					}
-					resolve();
-				}
-				catch(error) {
-					reject(error);
-				}
-			});
+		async fetchUser({commit}) {
+			let {data} = await axios.get("/server/ping.json");
+			commit('setAuthenticated', data.authenticated);
+			if(data.authenticated) {
+				({data} = await axios.get("/api/user/current.json"));
+				commit('setUser', data);
+			}
 		},
 
-		updateUser({commit, state}, user) {
-			return new Promise((resolve, reject) => {
-				axios.put(`/api/user/${state.currentUser.id}.json`, user)
-					.then(response => {
-						commit('setUser', response.data.user);
-						resolve();
-					})
-					.catch(error => {
-						reject(error);
-					});
-				}
-			);
+		async updateUser({commit, state}, user) {
+			let {data} = await axios.put(`/api/user/${state.currentUser.id}.json`, user);
+			commit('setUser', data.user);
 		},
 
-		login({state, dispatch}, payload) {
-			return new Promise(async (resolve, reject) => {
-				var params = new URLSearchParams();
-				params.append('j_username', payload.username);
-				params.append('j_password', payload.password);
-				params.append('j_email', '');
-				params.append('remember_me', payload.remember_me ? "on" : "off");
-				try {
-					await axios.post("j_spring_security_check", params, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
-						.catch(() => {}); // TODO: remove catch (temporary patch for CORS)
-					await dispatch("fetchUser");
-					if(state.authenticated) {
-						resolve();
-					}
-					else {
-						reject();
-					}
-				}
-				catch(error) {
-					reject(error);
-				}
-			});
+		async login({state, dispatch}, payload) {
+			var params = new URLSearchParams();
+			params.append('j_username', payload.username);
+			params.append('j_password', payload.password);
+			params.append('j_email', '');
+			params.append('remember_me', payload.remember_me ? "on" : "off");
+			await axios.post("j_spring_security_check", params, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+				.catch(() => {}); // TODO: remove catch (temporary patch for CORS)
+			await dispatch("fetchUser");
+			if(!state.authenticated) {
+				throw Error("Invalid credentials"); // TODO: custom error
+			}
 		},
 
-		logout({commit}) {
-			return new Promise(async (resolve, reject) => {
-				axios.put(`j_spring_security_logout`)
-					.then(response => {
-						commit('logout');
-						resolve();
-					})
-					.catch(error => {
-						reject(error);
-					});
-				}
-			);
+		async logout({commit}) {
+			await axios.put(`j_spring_security_logout`);
+			commit('logout');
 		}
 	}
 });
