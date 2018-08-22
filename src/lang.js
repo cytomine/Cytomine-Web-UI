d7cb8e3250b187365fd3437dfacf23ea7ec4ec62
eburtin <elodie.burtin@cytomine.coop>
2018-08-22 13:53:03 +0200

    Ignore JSON translation files generated by i18n-csv2json

--- src/lang.js
+++ src/lang.js
@@ -4,10 +4,10 @@ import VueI18n from 'vue-i18n';
 Vue.use(VueI18n);
 
 export default new VueI18n({
-	locale: 'en',
-	fallbackLocale: 'en',
-	messages: {
-		'en': require('./locales/en.i18n.json'),
-		'fr': require('./locales/fr.i18n.json')
-	}
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+        'en': require('./locales/json/en.i18n.json'),
+        'fr': require('./locales/json/fr.i18n.json')
+    }
 });
