f49d66553c521538ba7c8577cccf691b0e832874
eburtin <elodie.burtin@cytomine.coop>
2019-03-13 18:50:03 +0100

    Initial commit for forgotten credentials

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -1,34 +1,84 @@
 <template>
     <div class="panel">
-        <p class="panel-heading">
-            <i class="fas fa-user" aria-hidden="true"></i>
-            {{$t("login")}}
-        </p>
-        <div class="panel-block">
-            <form>
-                <b-field :label="$t('username')">
-                    <b-input v-model="username"></b-input>
-                </b-field>
+        <template v-if="forgotUsername">
+            <p class="panel-heading">
+                <i class="fas fa-user" aria-hidden="true"></i>
+                {{$t("forgot-username")}}
+            </p>
+            <div class="panel-block">
+                <form>
+                    <b-field :label="$t('email')">
+                        <b-input v-model="email"></b-input>
+                    </b-field>
 
-                <b-field :label="$t('password')">
-                    <b-input type="password" v-model="password"></b-input>
-                </b-field>
-
-                <b-field>
-                    <b-checkbox v-model="rememberMe" type="is-info">{{$t("remember-me")}}</b-checkbox>
-                </b-field>
-
-                <b-field grouped position="is-right">
-                    <div class="control">
-                        <button class="button is-link" @click="login()"> {{$t("button-save")}}</button>
+                    <div class="buttons">
+                        <button class="button" type="button" @click="forgotUsername = false"> {{$t("button-cancel")}}</button>
+                        <button class="button is-link" @click="sendUsername()"> {{$t("button-get-username")}}</button>
                     </div>
-                </b-field>
-            </form>
-        </div>
+                </form>
+            </div>
+        </template>
+        <template v-else-if="forgotPassword">
+            <p class="panel-heading">
+                <i class="fas fa-user" aria-hidden="true"></i>
+                {{$t("forgot-password")}}
+            </p>
+            <div class="panel-block">
+                <form>
+                    <b-field :label="$t('username')">
+                        <b-input v-model="username"></b-input>
+                    </b-field>
+
+                    <div class="buttons">
+                        <button class="button" type="button" @click="forgotPassword = false"> {{$t("button-cancel")}}</button>
+                        <button class="button is-link" @click="resetPassword()"> {{$t("button-reset-password")}}</button>
+                    </div>
+                </form>
+            </div>
+        </template>
+        <template v-else>
+            <p class="panel-heading">
+                <i class="fas fa-user" aria-hidden="true"></i>
+                {{$t("login")}}
+            </p>
+            <div class="panel-block">
+                <form>
+                    <b-field :label="$t('username')">
+                        <b-input v-model="username"></b-input>
+                    </b-field>
+
+                    <b-field :label="$t('password')">
+                        <b-input type="password" v-model="password"></b-input>
+                    </b-field>
+
+                    <div class="columns">
+                        <div class="column">
+                            <b-field>
+                                <b-checkbox v-model="rememberMe" type="is-info">{{$t("remember-me")}}</b-checkbox>
+                            </b-field>
+                        </div>
+
+                        <div class="column">
+                            <b-field grouped position="is-right">
+                                <div class="control">
+                                    <button class="button is-link" @click="login()"> {{$t("button-login")}}</button>
+                                </div>
+                            </b-field>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <i18n path="forgot-credentials" tag="p" class="is-size-7 has-text-centered forgot-credentials">
+                <a place="username" @click="forgotUsername = true">{{ $t("username") }}</a>
+                <a place="password" @click="forgotPassword = true">{{ $t("password") }}</a>
+            </i18n>
+        </template>
     </div>
 </template>
 
 <script>
+
+import {Cytomine} from "cytomine-client";
 
 export default {
     name: "login",
@@ -36,7 +86,11 @@ export default {
         return {
             username: "",
             password: "",
-            rememberMe: true
+            email: "",
+            rememberMe: true,
+
+            forgotUsername: false,
+            forgotPassword: false
         };
     },
     methods: {
@@ -54,6 +108,26 @@ export default {
             catch(error) {
                 this.$notify({type: "error", text: error.response.data.message});
             }
+        },
+        async sendUsername() {
+            try {
+                await Cytomine.instance.forgotUsername(this.email);
+                this.$notify({type: "success", text: this.$t("notif-success-forgot-username")});
+                this.forgotUsername = false;
+            }
+            catch(error) {
+                this.$notify({type: "error", text: this.$t("notif-error-forgot-username")});
+            }
+        },
+        async resetPassword() {
+            try {
+                await Cytomine.instance.forgotPassword(this.username);
+                this.$notify({type: "success", text: this.$t("notif-success-forgot-username")});
+                this.forgotPassword = false;
+            }
+            catch(error) {
+                this.$notify({type: "error", text: this.$t("notif-error-forgot-username")});
+            }
         }
     }
 };
@@ -62,8 +136,25 @@ export default {
 <style scoped>
 .panel {
     width: 100%;
-    max-width: 500px;
+    max-width: 600px;
     margin: auto;
     margin-top: 50px;
+}
+
+.columns, .buttons {
+    margin-top: 0.5em;
+}
+
+.buttons {
+    justify-content: flex-end;
+    margin-top: 1em;
+}
+
+.panel-block {
+    margin-bottom: 1em;
+}
+
+.forgot-credentials a {
+    text-transform: lowercase;
 }
 </style>
