f7154ddcc642237966daa906170d668719838dc5
eburtin <elodie.burtin@cytomine.coop>
2019-03-25 14:20:19 +0100

    Fix delete button in project information page

--- src/components/project/ProjectInformation.vue
+++ src/components/project/ProjectInformation.vue
@@ -4,13 +4,13 @@
     <p>{{ $t("insufficient-permission") }}</p>
 </div>
 <div v-else class="box">
-    <project-details :project="project" :excluded-properties="['imagesPreview']">
-    </project-details>
+    <project-details :project="project" :excluded-properties="['imagesPreview']" @delete="deleteProject()" />
 </div>
 </template>
 
 <script>
 import ProjectDetails from "./ProjectDetails";
+import {Project} from "cytomine-client";
 
 export default {
     name: "project-information",
@@ -21,6 +21,25 @@ export default {
         },
         configUI() {
             return this.$store.state.project.configUI;
+        }
+    },
+    methods: {
+        async deleteProject() {
+            try {
+                await Project.delete(this.project.id);
+                this.$notify({
+                    type: "success",
+                    text: this.$t("notif-success-project-deletion", {projectName: this.project.name})
+                });
+                this.$router.push("/projects");
+            }
+            catch(error) {
+                console.log(error);
+                this.$notify({
+                    type: "error",
+                    text: this.$t("notif-error-project-deletion", {projectName: this.project.name})
+                });
+            }
         }
     }
 };
