6d09b7093aa4ab4ab0b35cbc05fb5267496b15ec
eburtin <elodie.burtin@cytomine.coop>
2019-05-13 13:56:08 +0200

    Username now returned in job creation response => remove related hack

--- src/components/job/ListJobs.vue
+++ src/components/job/ListJobs.vue
@@ -220,7 +220,6 @@ export default {
       this.sort = {field, order};
     },
     addJob(job) {
-      job.username = this.currentUser.username; // HACK because not correctly returned by core
       this.jobs.unshift(job);
       this.openedDetails = [job.id, ...this.openedDetails];
     },
