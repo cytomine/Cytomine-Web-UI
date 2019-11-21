e137cce481559d2db13d86592532b6505cc278fa
Ulysse Rubens <ulyrubens@hotmail.com>
2019-11-21 12:42:54 +0100

    Show full name of software in Add job modal
    Signed-off-by: Ulysse Rubens <ulyrubens@hotmail.com>

--- src/components/job/AddJobModal.vue
+++ src/components/job/AddJobModal.vue
@@ -21,7 +21,7 @@
         <strong>{{$t('algorithm')}}</strong>
       </div>
       <div class="column">
-        <cytomine-multiselect v-model="selectedSoftware" :options="softwares" track-by="id" label="name" />
+        <cytomine-multiselect v-model="selectedSoftware" :options="softwares" track-by="id" label="fullName" />
       </div>
     </div>
     <template v-if="selectedSoftware">
@@ -40,8 +40,8 @@
             v-model="param.value"
           />
 
-          <tr class="row-separator" v-if="optionalParams.length > 0">
-            <td colspan="2">
+          <tr class="row-separator" v-show="optionalParams.length > 0">
+            <td colspan="3">
               {{$t('optional-parameters')}}
               <button class="button is-small" type="button" @click="showOptional = !showOptional">
                 {{$t(showOptional ? 'button-hide' : 'button-show')}}
@@ -57,8 +57,8 @@
             />
           </template>
 
-          <tr class="row-separator" v-if="prefilledParams.length > 0">
-            <td colspan="2">
+          <tr class="row-separator" v-show="prefilledParams.length > 0">
+            <td colspan="3">
               {{$t('prefilled-parameters')}}
               <button class="button is-small" type="button" @click="showPrefilled = !showPrefilled">
                 {{$t(showPrefilled ? 'button-hide' : 'button-show')}}
