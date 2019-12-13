cytomine/Cytomine-Web-UI

commit 652f12ae437aad490f9ea3a71d06d2191ab7a7c8

filter are empty by default

src/store/modules/list-projects.js
@@ -4,9 +4,9 @@ function getDefaultState() {
 
     filtersOpened: false,
     filters: {
-      selectedOntologies: null,
-      selectedRoles: null,
-      selectedTags: null,
+      selectedOntologies: [],
+      selectedRoles: [],
+      selectedTags: [],
       boundsMembers: null,
       boundsImages: null,
       boundsUserAnnotations: null,
@@ -67,7 +67,7 @@ export default {
 
   getters: {
     nbActiveFilters: state => {
-      return Object.values(state.filters).filter(val => val).length; // count the number of not null values
+      return Object.values(state.filters).filter(val => val && val.length > 0).length; // count the number of not null values
     }
   }
 };

src/store/modules/project_modules/list-images.js
@@ -7,10 +7,11 @@ export default {
 
       filtersOpened: false,
       filters: {
-        formats: null,
-        vendors: null,
-        magnifications: null,
-        resolutions: null,
+        formats: [],
+        vendors: [],
+        selectedTags: [],
+        magnifications: [],
+        resolutions: [],
         boundsWidth: null,
         boundsHeight: null,
         boundsUserAnnotations: null,
@@ -62,7 +63,7 @@ export default {
 
   getters: {
     nbActiveFilters: state => {
-      return Object.values(state.filters).filter(val => val).length; // count the number of not null values
+      return Object.values(state.filters).filter(val => val && val.length > 0).length; // count the number of not null values
     }
   }
 };

src/store/modules/project_modules/list-jobs.js
@@ -4,9 +4,9 @@ export default {
   state() {
     return {
       filters: {
-        softwares: null,
-        launchers: null,
-        statuses: null
+        softwares: [],
+        launchers: [],
+        statuses: []
       },
       executionDate: null,
 

commit 3455a769a4b5322b9aba8845277a462226b08af0

Filters: fix regression introduced by 652f12ae437aad490f9ea3a71d06d2191ab7a7c8
Signed-off-by: Ulysse Rubens <ulyrubens@hotmail.com>

src/components/image/ListImages.vue
@@ -159,6 +159,7 @@
 
       <cytomine-table
         :collection="imageCollection"
+        :is-empty="nbEmptyFilters > 0"
         :currentPage.sync="currentPage"
         :perPage.sync="perPage"
         :openedDetailed.sync="openedDetails"
@@ -378,6 +379,9 @@ export default {
 
     nbActiveFilters() {
       return this.$store.getters[this.storeModule + '/nbActiveFilters'];
+    },
+    nbEmptyFilters() {
+      return this.$store.getters[this.storeModule + '/nbEmptyFilters'];
     },
 
     currentPage: sync('currentPage', storeOptions),

src/components/job/ListJobs.vue
@@ -90,6 +90,7 @@
 
       <cytomine-table
         :collection="jobCollection"
+        :is-empty="nbEmptyFilters > 0"
         :currentPage.sync="currentPage"
         :perPage.sync="perPage"
         :openedDetailed.sync="openedDetails"
@@ -220,6 +221,10 @@ export default {
     selectedDate: sync('executionDate', storeOptions),
     selectedStatus: localSyncMultiselectFilter('statuses', 'availableStatus'),
     selectedFavorites: localSyncMultiselectFilter('favorites', 'availableFavorites'),
+
+    nbEmptyFilters() {
+      return this.$store.getters[this.storeModule + '/nbEmptyFilters'];
+    },
 
     jobCollection() {
       let collection = new JobCollection({

src/components/project/ListProjects.vue
@@ -135,6 +135,7 @@
 
       <cytomine-table
         :collection="projectCollection"
+        :is-empty="nbEmptyFilters > 0"
         class="table-projects"
         :currentPage.sync="currentPage"
         :perPage.sync="perPage"
@@ -304,6 +305,9 @@ export default {
 
     nbActiveFilters() {
       return this.$store.getters['listProjects/nbActiveFilters'];
+    },
+    nbEmptyFilters() {
+      return this.$store.getters['listProjects/nbEmptyFilters'];
     },
 
     selectedOntologiesIds() {

src/components/utils/CytomineTable.vue
@@ -85,7 +85,8 @@ export default {
   name: 'cytomine-table',
   props: {
     collection: Object,
-    perPageOptions: {type: Array, default: () => [10, 25, 50, 100]},
+    isEmpty: {type: Boolean, default: false},
+    perPageOptions: {type: Array, default: () => [5, 10, 25, 50, 100]},
     perPage: {type: Number, default: 25},
     currentPage: {type: Number, default: 1},
     detailed: {type: Boolean, default: true},
@@ -167,7 +168,14 @@ export default {
       }
 
       try {
-        let data = await this.internalCollection.fetchPage(this.internalCurrentPage - 1);
+        let data = {array: [], totalNbItems: 0};
+        if (this.isEmpty) {
+          this.internalCurrentPage = 1;
+        }
+        else {
+          data = await this.internalCollection.fetchPage(this.internalCurrentPage - 1);
+        }
+
         this.data = data.array;
         this.total = data.totalNbItems;
 

src/store/modules/list-projects.js
@@ -20,9 +20,9 @@ function getDefaultState() {
 
     filtersOpened: false,
     filters: {
-      selectedOntologies: [],
-      selectedRoles: [],
-      selectedTags: [],
+      selectedOntologies: null,
+      selectedRoles: null,
+      selectedTags: null,
       boundsMembers: null,
       boundsImages: null,
       boundsUserAnnotations: null,
@@ -83,7 +83,11 @@ export default {
 
   getters: {
     nbActiveFilters: state => {
-      return Object.values(state.filters).filter(val => val && val.length > 0).length; // count the number of not null values
+      return Object.values(state.filters).filter(val => val).length; // count the number of not null values
+    },
+
+    nbEmptyFilters: state => {
+      return Object.values(state.filters).filter(val => val && val.length === 0).length;
     }
   }
 };

src/store/modules/project_modules/list-images.js
@@ -23,11 +23,11 @@ export default {
 
       filtersOpened: false,
       filters: {
-        formats: [],
-        vendors: [],
-        selectedTags: [],
-        magnifications: [],
-        resolutions: [],
+        formats: null,
+        vendors: null,
+        selectedTags: null,
+        magnifications: null,
+        resolutions: null,
         boundsWidth: null,
         boundsHeight: null,
         boundsUserAnnotations: null,
@@ -79,7 +79,11 @@ export default {
 
   getters: {
     nbActiveFilters: state => {
-      return Object.values(state.filters).filter(val => val && val.length > 0).length; // count the number of not null values
+      return Object.values(state.filters).filter(val => val).length; // count the number of not null values
+    },
+
+    nbEmptyFilters: state => {
+      return Object.values(state.filters).filter(val => val && val.length === 0).length;
     }
   }
 };

src/store/modules/project_modules/list-jobs.js
@@ -20,10 +20,10 @@ export default {
   state() {
     return {
       filters: {
-        softwares: [],
-        launchers: [],
-        statuses: [],
-        favorites: []
+        softwares: null,
+        launchers: null,
+        statuses: null,
+        favorites: null
       },
       executionDate: null,
 
@@ -62,6 +62,16 @@ export default {
 
     setOpenedDetails(state, value) {
       state.openedDetails = value;
+    }
+  },
+
+  getters: {
+    nbActiveFilters: state => {
+      return Object.values(state.filters).filter(val => val).length; // count the number of not null values
+    },
+
+    nbEmptyFilters: state => {
+      return Object.values(state.filters).filter(val => val && val.length === 0).length;
     }
   }
 };