cytomine/Cytomine-Web-UI

commit c8680c88995321c76e61ee85892e7e4cc933ac28

Add creation date in ImageDetails.vue

src/components/image/ImageDetails.vue
@@ -34,6 +34,12 @@
           <image-status :image="image" />
         </td>
       </tr>
+      <tr v-if="isPropDisplayed('created')">
+        <td class="prop-label">{{$t('created-on')}}</td>
+        <td class="prop-content">
+          {{ Number(image.created) | moment('ll') }}
+        </td>
+      </tr>
       <tr v-if="isPropDisplayed('numberOfAnnotations')">
         <td class="prop-label">{{$t('user-annotations')}}</td>
         <td class="prop-content" colspan="3">

commit d81e70bac8d6eae1da3fb1017fb42dfce220ce98

Fix glitch in image details introduced by c8680c88995321c76e61ee85892e7e4cc933ac28

src/components/image/ImageDetails.vue
@@ -36,7 +36,7 @@
       </tr>
       <tr v-if="isPropDisplayed('created')">
         <td class="prop-label">{{$t('created-on')}}</td>
-        <td class="prop-content">
+        <td class="prop-content" colspan="3">
           {{ Number(image.created) | moment('ll') }}
         </td>
       </tr>