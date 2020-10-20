54871f10f68ab68106fcbff740fed4b11d756341
Ulysse Rubens <urubens@uliege.be>
2020-10-20 15:58:04 +0200

    USe backend search for image selector in viewer

--- src/components/viewer/ImageSelector.vue
+++ src/components/viewer/ImageSelector.vue
@@ -16,13 +16,14 @@
 <div>
   <div class="image-selector-wrapper" v-show="imageSelectorEnabled">
     <b-loading :is-full-page="false" :active="loading" />
-    <template v-if="!loading">
       <div class="header">
-        <b-input class="search-images" v-model="searchString" :placeholder="$t('search-placeholder')"
+        <b-input class="search-images" :value="searchString" @input="debounceSearchString"
+          :placeholder="$t('search-placeholder')"
           type="search" icon="search"
         />
         <button class="delete" @click="imageSelectorEnabled = false"></button>
       </div>
+    <template v-if="!loading">
       <div class="content-wrapper" v-if="error">
         <b-message type="is-danger" has-icon icon-size="is-small">
           <h2> {{ $t('error') }} </h2>
@@ -30,7 +31,7 @@
         </b-message>
       </div>
       <div v-else class="image-selector">
-        <div class="card" v-for="image in displayedImages" :key="image.id">
+        <div class="card" v-for="image in images" :key="image.id">
           <a class="card-image" @click="addImage(image)" :style="'background-image: url(' + image.preview + ')'"></a>
           <div class="card-content">
             <div class="content">
@@ -41,7 +42,7 @@
           </div>
         </div>
 
-        <button class="button" v-if="nbImagesDisplayed < filteredImages.length" @click="more()">
+        <button class="button" v-if="nbImagesDisplayed < nbFilteredImages" @click="more()">
           {{$t('button-more')}}
         </button>
 
@@ -65,7 +66,7 @@ import {get} from '@/utils/store-helpers';
 
 import ImageName from '@/components/image/ImageName';
 import {ImageInstanceCollection} from 'cytomine-client';
-import {getWildcardRegexp} from '@/utils/string-utils';
+import _ from 'lodash';
 
 export default {
   name: 'image-selector',
@@ -75,6 +76,7 @@ export default {
       images: [],
       searchString: '',
       nbImagesDisplayed: 20,
+      nbFilteredImages: 0,
       loading: true,
       error: false
     };
@@ -92,21 +94,19 @@ export default {
         this.$store.commit(this.viewerModule + 'setImageSelector', value);
       }
     },
-    filteredImages() { // TODO: in backend
-      let filtered = this.images;
-
-      if(this.searchString) {
-        let regexp = getWildcardRegexp(this.searchString);
-        filtered =  filtered.filter(image => regexp.test(image.instanceFilename));
-      }
-
-      return filtered;
+  },
+  watch: {
+    searchString() {
+      this.fetchImages();
     },
-    displayedImages() {
-      return this.filteredImages.slice(0, this.nbImagesDisplayed);
+    nbImagesDisplayed() {
+      this.fetchImages();
     }
   },
   methods: {
+    debounceSearchString: _.debounce(async function(value) {
+      this.searchString = value;
+    }, 500),
     async addImage(image) {
       try {
         await image.fetch(); // refetch image to ensure we have latest version
@@ -117,6 +117,31 @@ export default {
         console.log(error);
         this.$notify({type: 'error', text: this.$t('notif-error-add-viewer-image')});
       }
+    },
+    async fetchImages() {
+      this.loading = true;
+      try {
+        let collection = new ImageInstanceCollection({
+          filterKey: 'project',
+          filterValue: this.project.id,
+          max: this.nbImagesDisplayed
+        });
+
+        if(this.searchString) {
+          collection['name'] = {
+            ilike: encodeURIComponent(this.searchString)
+          };
+        }
+
+        let data = (await collection.fetchPage(0));
+        this.images = data.array;
+        this.nbFilteredImages = data.totalNbItems;
+      }
+      catch(error) {
+        console.log(error);
+        this.error = true;
+      }
+      this.loading = false;
     },
 
     more() {
@@ -134,17 +159,7 @@ export default {
     }
   },
   async created() {
-    try {
-      this.images = (await ImageInstanceCollection.fetchAll({
-        filterKey: 'project',
-        filterValue: this.project.id
-      })).array; // TODO: should not load full array, should be done with backend
-    }
-    catch(error) {
-      console.log(error);
-      this.error = true;
-    }
-    this.loading = false;
+    await this.fetchImages();
   },
   mounted() {
     this.$eventBus.$on('shortkeyEvent', this.shortkeyHandler);
