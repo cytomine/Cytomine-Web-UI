f48857076e80cca7cbd4713b900cf003a1ec292e
Ulysse Rubens <urubens@uliege.be>
2021-01-14 10:24:03 +0100

    feat: Add new shortcuts to toggle panels in all views
    (cherry picked from commit f0f94244e683340fb0c11dce97dffa0890dd711e)

--- src/components/viewer/DrawTools.vue
+++ src/components/viewer/DrawTools.vue
@@ -848,7 +848,7 @@ export default {
     },
 
     shortkeyHandler(key) {
-      if(!this.isActiveImage) { // shortkey should only be applied to active map
+      if(key !== 'toggle-all-current' && !this.isActiveImage) { // shortkey should only be applied to active map
         return;
       }
 
@@ -949,6 +949,7 @@ export default {
           }
           return;
         case 'toggle-current':
+        case 'toggle-all-current':
           if (this.configUI['project-explore-annotation-main'] && this.selectedFeature) {
             this.displayAnnotDetails = !this.displayAnnotDetails;
           }
