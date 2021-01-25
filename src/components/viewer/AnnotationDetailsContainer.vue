5c43a41bebbecd6b8ba6e4029dfaae7f0b5f82a4
Ulysse Rubens <urubens@uliege.be>
2021-01-25 14:57:18 +0100

    Experimental feature: do not re-force to display annot details on newly selected feature

--- src/components/viewer/AnnotationDetailsContainer.vue
+++ src/components/viewer/AnnotationDetailsContainer.vue
@@ -156,7 +156,7 @@ export default {
   watch: {
     selectedFeature() {
       if(this.selectedFeature) {
-        this.displayAnnotDetails = true;
+        // this.displayAnnotDetails = true;
         let targetAnnot = this.imageWrapper.selectedFeatures.showComments;
         this.showComments = (targetAnnot === this.annot.id);
         if(targetAnnot !== null) {
