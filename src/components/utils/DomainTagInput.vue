236cd54bf98cd740e78fa96b6b81432c9016697f
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2021-05-01 11:08:27 +0200

    modal dropdownlist content appended to body. CSS changes to made content visible. Related to https://github.com/buefy/buefy/issues/1163

--- src/components/utils/DomainTagInput.vue
+++ src/components/utils/DomainTagInput.vue
@@ -20,6 +20,7 @@
   :data="filteredDomains"
   autocomplete
   :open-on-focus="true"
+  :append-to-body="true"
   :field="displayedProperty"
   :placeholder="$t(placeholder)"
   @typing="val => searchString = val"
