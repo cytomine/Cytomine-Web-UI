73b249edcc7f494698d50ad48e0cb85f29cf315e
lrollus <loic.rollus@cytomine.com>
2021-07-15 09:50:12 +0200

    fix

--- scripts/ciBuildDockerImage.sh
+++ scripts/ciBuildDockerImage.sh
@@ -8,7 +8,7 @@ echo "************************************** Publish docker ********************
 file='./ci/version'
 VERSION_NUMBER=$(<"$file")
 echo "$1"
-docker build –no-cache --rm -f scripts/docker/web-ui/Dockerfile --build-arg CURRENT_BRANCH=$1 -t  cytomine/web_ui:v$VERSION_NUMBER ./scripts/docker/web-ui
+docker build --no-cache --rm -f scripts/docker/web-ui/Dockerfile --build-arg CURRENT_BRANCH=$1 -t  cytomine/web_ui:v$VERSION_NUMBER ./scripts/docker/web-ui
 
 docker push cytomine/web_ui:v$VERSION_NUMBER
 
