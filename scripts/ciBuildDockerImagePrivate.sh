d6b49875dc30c0b8f71097336a891192055af72c
lrollus <loic.rollus@cytomine.com>
2021-08-13 09:55:35 +0200

    corporate build

---
+++ scripts/ciBuildDockerImagePrivate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o xtrace
+set -o errexit
+
+echo "************************************** Publish docker ******************************************"
+
+file='./ci/version'
+VERSION_NUMBER=$(<"$file")
+echo "$1"
+docker build --no-cache --rm -f scripts/docker/web-ui/Dockerfile --build-arg CURRENT_BRANCH=$1 -t  repository.cytom.in:5004/web_ui-$CUSTOMER:v$VERSION_NUMBER ./scripts/docker/web-ui
+
+docker push repository.cytom.in:5004/web_ui-$CUSTOMER:v$VERSION_NUMBER
+
+docker rmi repository.cytom.in:5004/web_ui-$CUSTOMER:v$VERSION_NUMBER
+
+echo "Image: repository.cytom.in:5004/web_ui-$CUSTOMER:v$VERSION_NUMBER"
+
+echo "In order to download image on a new host:"
+echo "1. Create/Modify /etc/docker/daemon.json file with this: "
+echo "{ \"insecure-registries\":[\"repository.cytom.in:5004\"] }"
+echo "2. Restart docker service"
+echo "3. sudo docker login -u jenkins http://repository.cytom.in:5004"
