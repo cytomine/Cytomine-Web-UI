bdfa708262d5e868964a5f880e770ea6c572121d
lrollus <loic.rollus@cytomine.com>
2021-10-01 10:55:44 +0200

    public/private build

--- scripts/ciBuildDockerImage.sh
+++ scripts/ciBuildDockerImage.sh
@@ -8,7 +8,7 @@ echo "************************************** Publish docker ********************
 file='./ci/version'
 VERSION_NUMBER=$(<"$file")
 echo "$1"
-docker build --no-cache --rm -f scripts/docker/web-ui/Dockerfile --build-arg CURRENT_BRANCH=$1 -t  cytomine/web_ui:v$VERSION_NUMBER ./scripts/docker/web-ui
+docker build --no-cache --rm -f scripts/docker/web-ui/Dockerfile -t  cytomine/web_ui:v$VERSION_NUMBER .
 
 docker push cytomine/web_ui:v$VERSION_NUMBER
 
