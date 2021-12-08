6bce5caf13b6902f1f63f57d61c386cf97606539
Ulysse Rubens <urubens@uliege.be>
2021-12-08 09:06:20 +0100

    Update Cytomine components info for pims

--- src/components/navbar/AboutCytomineModal.vue
+++ src/components/navbar/AboutCytomineModal.vue
@@ -152,7 +152,7 @@ export default {
           'name': component,
           'enabled': constants[`${component}_ENABLED`] == null || constants[`${component}_ENABLED`],
           'version': constants[`${component}_VERSION`],
-          'namespace': constants[`${component}_NAMESPACE`]
+          'namespace': constants[`${component}_NAMESPACE`] || '-'
         };
       });
     }
