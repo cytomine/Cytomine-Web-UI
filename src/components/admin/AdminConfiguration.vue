4a17e0bff12fc469d004a87f3692e217bf8baf14
Paul Barthe <p.barthe441@hotmail.com>
2022-02-07 17:07:39 +0100

    bidirectional color picker

--- src/components/admin/AdminConfiguration.vue
+++ src/components/admin/AdminConfiguration.vue
@@ -20,6 +20,45 @@
   <p class="has-text-right">
     <button class="button is-link" @click="save">{{$t('button-save')}}</button>
   </p>
+
+  <hr/>
+  <h2>{{$t('top-menu')}}</h2>
+  <div class="columns">
+    <div class="column is-one-quarter" style="padding-left:3.5em;">
+      <h3>{{$t('top-menu-color')}}</h3>
+    </div>
+    <div class="column is-one-quarter">
+      <div class="columns">
+        <div class="column is-four-fifths">
+          <b-input
+            v-on:updateColor="updateColor($event.target.value)"
+            v-model="topMenuColorConfig.value"
+            placeholder=""
+          />
+        </div>
+        <button class="button color-picker-button" :style="`background-color:${topMenuColorConfig.value};`" @click="openModal()"></button>
+      </div>
+    </div>
+    <div class="column is-one-half">
+      <article class="message is-info is-small">
+        <section class="message-body">
+          <div class="media">
+            <div class="media-left">
+              <span class="icon is-small is-info"><i class="fas fa-info-circle"></i></span>
+            </div>
+            <div class="media-content">
+              {{$t('top-menu-color-description')}} <br/>
+              {{$t('top-menu-color-instruction')}} (<a href="https://www.w3schools.com/colors/colors_names.asp" target="_blank">Color list</a>)
+            </div>
+          </div>
+        </section>
+      </article>
+    </div>
+  </div>
+
+  <p class="has-text-right">
+    <button class="button is-link" @click="save">{{$t('button-save')}}</button>
+  </p>
 </div>
 </template>
 
@@ -27,6 +66,7 @@
 import CytomineQuillEditor from '@/components/form/CytomineQuillEditor';
 import {Configuration} from 'cytomine-client';
 import constants from '@/utils/constants.js';
+import ColorModal from './ColorModal';
 
 export default {
   name: 'admin-configuration',
@@ -45,13 +85,35 @@ export default {
         else {
           await this.welcomeConfig.save();
         }
-        this.$notify({type: 'success', text: this.$t('notif-success-welcome-message-update')});
+        if(!this.topMenuColorConfig.value && this.topMenuColorConfig.id!=null) {
+          await this.topMenuColorConfig.delete();
+        }
+        else if (this.topMenuColorConfig.value) {
+          await this.topMenuColorConfig.save();
+        }
+        this.$notify({type: 'success', text: this.$t('notif-success-configuration-update')});
       }
       catch(error) {
         console.log(error);
         this.$notify({type: 'error', text: this.$t('notif-error-welcome-message-update')});
       }
-    }
+    },
+    updateColor(color) {
+      this.topMenuColorConfig.value = color;
+    },
+    openModal() {
+      this.$buefy.modal.open({
+        parent: this,
+        component: ColorModal,
+        props: {
+          topMenuColor: this.topMenuColorConfig ? this.topMenuColorConfig.value : null,
+        },
+        events: {
+          updateColor: this.updateColor,
+        },
+        hasModalCard: true
+      });
+    },
   },
   async created() {
     try {
@@ -73,4 +135,10 @@ export default {
 .button {
   margin-top: 1em;
 }
+
+.color-picker-button {
+  margin-top: 0.75em;
+  width: 2.25em;
+}
+
 </style>
