1022eaa869c3a538787ebcf425f62ce2639736bb
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2022-03-23 17:35:46 +0100

    clean EDU admin config page after merges

--- src/components/admin/AdminConfiguration.vue
+++ src/components/admin/AdminConfiguration.vue
@@ -18,37 +18,6 @@
   <h2>{{$t('welcome-message')}}</h2>
   <cytomine-quill-editor v-model="welcomeConfig.value" />
 
-  <hr/>
-  <h2>{{$t('mail-settings')}}</h2>
-  <div class="columns">
-    <div class="column is-one-quarter" style="padding-left:3.5em;">
-      <h3>{{$t('shared-annotation-mail-mode')}}</h3>
-    </div>
-    <div class="column is-one-quarter">
-      <cytomine-multiselect
-        v-model="sharedAnnotationMailMode.value"
-        :options="['classic', 'restricted']"
-        :allow-empty="false"
-        :searchable="false"
-      />
-
-    </div>
-    <div class="column is-one-half">
-      <article class="message is-info is-small">
-        <section class="message-body">
-          <div class="media">
-            <div class="media-left">
-              <span class="icon is-small is-info"><i class="fas fa-info-circle"></i></span>
-            </div>
-            <div class="media-content">
-              {{$t('shared-annotation-mail-mode-description')}}
-            </div>
-          </div>
-        </section>
-      </article>
-    </div>
-  </div>
-
 
   <hr/>
   <h2>{{$t('top-menu')}}</h2>
@@ -172,32 +141,6 @@
   <h2>{{$t('temporary-data-settings')}}</h2>
   <div class="columns">
     <div class="column is-one-quarter" style="padding-left:3.5em;">
-      <h3>{{$t('to-delete-at-configuration')}}</h3>
-    </div>
-    <div class="column is-one-quarter">
-      <b-input
-        v-model="toDeleteAtDelayConfig.value"
-        placeholder=""
-      />
-    </div>
-    <div class="column is-one-half">
-      <article class="message is-info is-small">
-        <section class="message-body">
-          <div class="media">
-            <div class="media-left">
-              <span class="icon is-small is-info"><i class="fas fa-info-circle"></i></span>
-            </div>
-            <div class="media-content">
-              {{$t('to-delete-at-configuration-description')}}
-            </div>
-          </div>
-        </section>
-      </article>
-    </div>
-  </div>
-
-  <div class="columns">
-    <div class="column is-one-quarter" style="padding-left:3.5em;">
       <h3>{{$t('activities-time-to-live-in-hours')}}</h3>
     </div>
     <div class="column is-one-quarter">
@@ -281,6 +224,7 @@ export default {
       fetching: true,
       welcomeConfig: new Configuration({key: constants.CONFIG_KEY_WELCOME, value: '', readingRole: 'all'}),
       sharedAnnotationMailMode: new Configuration({key: constants.CONFIG_KEY_SHARE_ANNOTATION_EMAIL_MODE, value: 'classic', readingRole: 'all'}),
+      activitiesRetentionDelayConfig: new Configuration({key: constants.CONFIG_KEY_ACTIVITIES_RETENTION_DELAY_IN_DAYS, value: '0', readingRole: 'all'}),
       topMenuColorConfig: new Configuration({key: constants.CONFIG_KEY_COLOR_TOP_MENU, value: '', readingRole: 'all'}),
       topFontMenuColorConfig: new Configuration({key: constants.CONFIG_KEY_FONT_COLOR_TOP_MENU, value: '', readingRole: 'all'}),
       logoConfig: new Configuration({key: constants.CONFIG_KEY_LOGO_TOP_MENU, value: '', readingRole: 'all'}),
@@ -339,6 +283,13 @@ export default {
         else if (this.sharedAnnotationMailMode.value) {
           await this.sharedAnnotationMailMode.save();
         }
+        if(!this.activitiesRetentionDelayConfig.value && this.activitiesRetentionDelayConfig.id!=null) {
+          await this.activitiesRetentionDelayConfig.delete();
+        }
+        else if (this.activitiesRetentionDelayConfig.value) {
+          await this.activitiesRetentionDelayConfig.save();
+        }
+
         if(!this.topMenuColorConfig.value && this.topMenuColorConfig.id!=null) {
           await this.topMenuColorConfig.delete();
         }
@@ -410,6 +361,7 @@ export default {
   async created() {
     try {
       await this.welcomeConfig.fetch();
+      await this.activitiesRetentionDelayConfig.fetch();
     }
     catch(error) {
       // no welcome message currently set
