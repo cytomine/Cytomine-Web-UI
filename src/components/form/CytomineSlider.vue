428fb7d933491ec0acaec8107d5efce9c8c594d5
Ulysse Rubens <urubens@uliege.be>
2022-04-20 15:39:26 +0200

    feat: Improve CytomineSlider: support for small size, better alignment, smart tooltip position

--- src/components/form/CytomineSlider.vue
+++ src/components/form/CytomineSlider.vue
@@ -20,10 +20,22 @@
   :tooltip="'always'"
   :tooltip-placement="tooltipPlacement"
   :lazy="lazy"
+  :dot-size="dotSize"
+  :contained="contained"
+  :enable-cross="false"
+  @dragging="internalValue = $event"
+  :class="[
+    {'vue-slider-margin-for-tooltip': tooltip && isArray && !smartTooltipPosition},
+    size
+  ]"
 >
-  <template #tooltip="{value, index}">
+  <template #tooltip="{value, index}" v-if="tooltip">
     <div
-      :class="['vue-slider-dot-tooltip-inner', `vue-slider-dot-tooltip-inner-${tooltipPlacement[index]}`]"
+      :class="[
+        'vue-slider-dot-tooltip-inner',
+        `vue-slider-dot-tooltip-inner-${tooltipPlacement[index]}`,
+        size
+        ]"
       @mousedown.stop
       @click.stop="startEdition(index)"
     >
@@ -40,6 +52,7 @@
           @hook:mounted="focus()"
           @blur="stopEdition(index)"
           @keyup.enter.native="stopEdition(index)"
+          :size="size"
         />
     </div>
   </template>
@@ -56,30 +69,58 @@ export default {
     value: {type: null},
     min: {type: Number, default: 0},
     max: {type: Number, default: 100},
+    interval: {type: Number},
     integerOnly: {type: Boolean, default: true},
-    lazy: {type: Boolean, default: true}
+    lazy: {type: Boolean, default: true},
+    tooltip: {type: Boolean, default: true},
+    size: {type: String, default: 'is-normal'}, // is-small, is-normal
+    contained: {type: Boolean, default: false}, // Whether the slider should be fully contained within its containing element.
+    smartTooltipPosition: {type: Boolean, default: false}
   },
   data() {
     return {
       indexEdited: null,
-      editedValue: 0
+      editedValue: 0,
+      internalValue: 0, // Bypass slider lazy update
     };
   },
   computed: {
     isArray() {
       return Array.isArray(this.value);
     },
+    range() {
+      return this.max - this.min;
+    },
     middle() {
-      return (this.max - this.min) /2;
+      return this.range / 2;
     },
     tooltipPlacement() {
-      if (this.isArray)
-        return ['left', 'right'];
+      if (this.isArray) {
+        if (this.smartTooltipPosition) {
+          const n = this.value.length + 1;
+          const values = (Array.isArray(this.internalValue)) ? this.internalValue : this.value;
+          return values.map((v, i) => (v >= this.range * (i+1)/n) ? 'left' : 'right');
+        }
+        return this.value.map((_, i) => (i === 0) ? 'left' : 'right');
+      }
 
-      if (this.value >= this.middle)
+      if (this.internalValue >= this.middle)
         return ['left'];
 
       return ['right'];
+    },
+    dotSize() {
+      switch (this.size) {
+        case 'is-small':
+          return 10.5;
+        default:
+          return 14;
+      }
+    }
+  },
+  watch: {
+    value() {
+      this.internalValue = this.value;
     }
   },
   methods: {
@@ -117,6 +158,9 @@ export default {
     focus() {
       this.$refs.inputSlider.focus();
     }
+  },
+  created() {
+    this.internalValue = this.value;
   }
 };
 </script>
@@ -128,14 +172,30 @@ export default {
   font-size: 0.9rem !important;
 }
 
-.vue-slider {
-  margin-left: 4rem;
-  margin-right: 6rem;
+.vue-slider-dot-tooltip-inner .control {
+  font-size: inherit;
 }
 
-.vue-slider-dot-tooltip input {
+.vue-slider-dot-tooltip-inner input {
   width: 4rem;
   height: 1.5rem;
   font-size: 0.8rem;
+}
+
+.vue-slider-dot-tooltip-inner.is-small {
+  font-size: 0.7rem !important;
+  padding: 1px 3px;
+  min-width: 10px;
+  border-radius: 3px;
+}
+.vue-slider-dot-tooltip-inner.is-small input {
+  width: 2rem;
+  height: 1rem;
+  font-size: 0.6rem;
+}
+
+.vue-slider-margin-for-tooltip {
+  margin-left: 4rem;
+  margin-right: 6rem;
 }
 </style>
