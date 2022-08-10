b2b5f821a441f262054d921ee07a6e3c067976c7
lrollus <loic.rollus@cytomine.com>
2022-08-10 08:24:32 +0200

    feat: storage viewer

--- src/components/storage/UploadedFileDetails.vue
+++ src/components/storage/UploadedFileDetails.vue
@@ -55,6 +55,10 @@
         <a v-if="node.data.macroURL" @click="slidePreview = node.data">
           {{$t('slide-preview')}}
         </a>
+        <span v-if="node.data.thumbURL">/</span>
+        <a v-if="node.data.image" @click="imageExplore = node.data">
+          {{$t('image-explore')}}
+        </a>
       </p>
     </template>
   </sl-vue-tree>
@@ -91,6 +95,13 @@
       <button class="button is-small" @click="slidePreview = null">{{$t('button-hide')}}</button>
     </h2>
     <img :src="slidePreview.macroURL">
+  </template>
+  <template v-else-if="imageExplore">
+    <h2>
+      {{$t('image-explore-of', {filename: imageExplore.originalFilename})}}
+      <button class="button is-small" @click="imageExplore = null">{{$t('button-hide')}}</button>
+    </h2>
+    <uploaded-file-details-viewer :image="abstractImage"></uploaded-file-details-viewer>
   </template>
 
   <template v-if="image && profileEnabled">
@@ -276,6 +287,7 @@ import MagnificationModal from '@/components/image/MagnificationModal';
 import CalibrationModal from '@/components/image/CalibrationModal';
 import HVMetadataModal from '@/components/image/HVMetadataModal';
 import RenameModal from '@/components/utils/RenameModal';
+import UploadedFileDetailsViewer from './UploadedFileDetailsViewer.vue';
 import filesize from 'filesize';
 import ProfileStatus from './ProfileStatus';
 
@@ -295,7 +307,8 @@ export default {
     CalibrationModal,
     RenameModal,
     HVMetadataModal,
-    UploadedFileStatus
+    UploadedFileStatus,
+    UploadedFileDetailsViewer
   },
   props: {
     file: Object // WARNING: the root of the tree must be the file or its direct parent
@@ -307,8 +320,10 @@ export default {
       uploadedFiles: [],
       nodes: [],
       image: null,
+      abstractImage: null,
       slidePreview: null,
       samplePreview: null,
+      imageExplore: null,
       error: false,
 
       nbUploadedFiles: 0,
@@ -347,13 +362,21 @@ export default {
     slidePreview(val) {
       if(val) {
         this.samplePreview = null; // if slide preview enabled, disable sample preview
+        this.imageExplore = null; // and image explore
       }
     },
     samplePreview(val) {
       if(val) {
         this.slidePreview = null; // if sample preview enabled, disable slide preview
+        this.imageExplore = null; // and image explore
       }
     },
+    imageExplore(val) {
+      if (val) {
+        this.slidePreview = null;
+        this.samplePreview = null;
+      }
+    }
   },
   methods: {
     findRoot() {
@@ -362,7 +385,7 @@ export default {
     async fetchAbstractImage() {
       if (this.file.image && (this.file.status === UFStatus.CONVERTED || this.file.status === UFStatus.DEPLOYED)) {
         try {
-          this.image = await AbstractImage.fetch(this.file.image);
+          this.abstractImage = await AbstractImage.fetch(this.file.image);
         }
         catch(error) {
           console.log(error);
@@ -473,11 +496,8 @@ export default {
   },
   async created() {
     this.findRoot();
-    this.image = new AbstractImage({id: this.file.image, class: 'be.cytomine.image.AbstractImage'});
     this.fetchAbstractImage();
-    /*this.makeTree();
-    this.abstractImage = new AbstractImage({id: this.file.image, class: 'be.cytomine.image.AbstractImage'});
-    this.abstractImage = await (this.abstractImage.fetch());*/
+    this.image = new AbstractImage({id: this.file.image, class: 'be.cytomine.image.AbstractImage'});
   }
 };
 </script>
---
+++ src/components/storage/UploadedFileDetailsViewer.vue
@@ -0,0 +1,109 @@
+<!-- Copyright (c) 2009-2022. Authors: see NOTICE file.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.-->
+
+<template>
+  <div v-if="!loading">
+    <vl-map
+      style="height: 500px"
+      :data-projection="projectionName"
+      :load-tiles-while-animating="true"
+      :load-tiles-while-interacting="true"
+    >
+      <vl-view
+        :center.sync="center"
+        :zoom.sync="zoom"
+        :max-zoom="maxZoom"
+        :max-resolution="maxResolution"
+        :extent="extent"
+        :projection="projectionName"
+      />
+
+      <vl-layer-tile :extent="extent" ref="baseLayer">
+        <vl-source-zoomify
+          :projection="projectionName"
+          :urls="baseLayerURLs"
+          :size="size"
+          :extent="extent"
+          crossOrigin="Anonymous"
+          :transition="0"
+        />
+      </vl-layer-tile>
+    </vl-map>
+  </div>
+</template>
+
+<script>
+
+import {addProj, createProj} from 'vuelayers/lib/ol-ext';
+import {AbstractSliceCollection} from 'cytomine-client';
+
+export default {
+  name: 'uploaded-file-details-viewer',
+  props: {
+    image: Object
+  },
+  data() {
+    return {
+      slice: null,
+      loading: true,
+      baseLayerURLs: null,
+      zoom: 2,
+      maxZoom: 0,
+      maxResolution: null,
+      center: null,
+      size: null,
+      extent: null,
+      projectionName: ''
+    };
+  },
+  methods: {
+    createProjection(imageId) {
+      this.projectionName = 'CYTO-' + imageId;
+      let projection = createProj({code: this.projectionName, units: 'pixels', extent: this.extent});
+      addProj(projection);
+    },
+    setBaseLayerURLs() {
+      let params = `&tileIndex={tileIndex}&z={z}&mimeType=${this.slice.mime}`;
+      this.baseLayerURLs = [`${this.image.imageServerUrl}/slice/tile?fif=${this.slice.path}${params}`];
+    },
+    setImageSize(){
+      this.size = [this.image.width, this.image.height];
+      this.extent = [0, 0, this.image.width, this.image.height];
+      this.center = [this.image.width/2, this.image.height/2];
+    },
+
+    async initViewVariables() {
+      let slices = (await AbstractSliceCollection.fetchAll({filterKey: 'abstractimage', filterValue: this.image.id})).array;
+      console.log('slices', slices);
+      console.log(this.image);
+      console.log(this.image.imageServerUrl);
+      this.slice = slices[0];
+      this.createProjection(this.image.id);
+
+      this.maxZoom = this.image.zoom;
+      this.maxResolution = Math.pow(2, this.image.zoom);
+
+      this.setBaseLayerURLs();
+      this.setImageSize();
+    },
+  },
+  async created() {
+    await this.initViewVariables();
+    this.loading = false;
+  }
+};
+</script>
+
+<style scoped>
+</style>
