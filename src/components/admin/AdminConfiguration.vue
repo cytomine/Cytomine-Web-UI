17c8041b2f774d8311f38ab8e5e06b50fe6aafe9
lrollus <loic.rollus@cytomine.com>
2022-10-06 10:44:13 +0200

    feat: lti configuration

--- src/components/admin/AdminConfiguration.vue
+++ src/components/admin/AdminConfiguration.vue
@@ -17,9 +17,9 @@
 <div>
   <h2>{{$t('welcome-message')}}</h2>
   <cytomine-quill-editor v-model="welcomeConfig.value" />
-  <p class="has-text-right">
-    <button class="button is-link" @click="save">{{$t('button-save')}}</button>
-  </p>
+<!--  <p class="has-text-right">-->
+<!--    <button class="button is-link" @click="save">{{$t('button-save')}}</button>-->
+<!--  </p>-->
 
   <hr/>
   <h2>{{$t('top-menu')}}</h2>
@@ -89,7 +89,63 @@
     </div>
   </div>
 
+  <hr/>
+  <h2>{{ $t('lti') }}</h2>
 
+  <h3 class="has-text-weight-bold lti-consumer">{{ $t('lti-consumers') }}</h3>
+  <button class="button is-small add-prop is-marginless"  @click="ltiConsumers.push({key:'', name:'',secret:'',usernameParameter:''})" key="showForm">
+    {{$t('button-add')}}
+  </button>
+  <div style="margin-top: 1em;" class="columns" v-for="(consumer, index) in ltiConsumers">
+    <div class="column is-one-fifth" style="padding-left:3.5em;">
+
+      <span class="has-text-weight-bold">{{$t('lti-consumer') + ' ' + index}} </span>
+
+<!--      <button class="edit is-small" :title="$t('button-edit')" @click="editedLTI=index">-->
+<!--        <i class="fas fa-pencil-alt"></i>-->
+<!--      </button>-->
+      <button class="delete is-small" :title="$t('button-delete')" @click="confirmLTIConsumerDeletion(index)"></button>
+<!--      <button class="button is-link" @click="ltiConsumers.splice(index, 1)">{{$t('button-remove')}}</button>-->
+
+    </div>
+
+    <div class="column is-one-fifth" style="padding-left:3.5em;">
+      <div class="column is-one-half" style="padding-left:3.5em;">
+        {{$t('lti-key')}}
+      </div>
+      <div class="column is-one-half">
+        <b-input v-model="ltiConsumers[index].key" placeholder=""/>
+<!--        <span v-if="editedLTI!=index" >{{ltiConsumers[index].key}}</span>-->
+      </div>
+    </div>
+
+    <div class="column is-one-fifth" style="padding-left:3.5em;">
+      <div class="column is-one-half" style="padding-left:3.5em;">
+        {{$t('lti-name')}}
+      </div>
+      <div class="column is-one-half">
+        <b-input v-model="ltiConsumers[index].name" placeholder=""/>
+      </div>
+    </div>
+
+    <div class="column is-one-fifth" style="padding-left:3.5em;">
+      <div class="column is-one-half" style="padding-left:3.5em;">
+        {{$t('lti-secret')}}
+      </div>
+      <div class="column is-one-half">
+        <b-input v-model="ltiConsumers[index].secret" placeholder=""/>
+      </div>
+    </div>
+
+    <div class="column is-one-fifth" style="padding-left:3.5em;">
+      <div class="column is-one-half" style="padding-left:3.5em;">
+        {{$t('lti-username-parameter')}}
+      </div>
+      <div class="column is-one-half">
+        <b-input v-model="ltiConsumers[index].usernameParameter" placeholder=""/>
+      </div>
+    </div>
+  </div>
   <p class="has-text-right">
     <button class="button is-link" @click="save">{{$t('button-save')}}</button>
   </p>
@@ -107,9 +163,12 @@ export default {
   components: {CytomineQuillEditor},
   data() {
     return {
+      ltiConsumers: [],
+      editedLTI: null,
       welcomeConfig: new Configuration({key: constants.CONFIG_KEY_WELCOME, value: '', readingRole: 'all'}),
       topMenuColorConfig: new Configuration({key: constants.CONFIG_KEY_COLOR_TOP_MENU, value: '', readingRole: 'all'}),
       topFontMenuColorConfig: new Configuration({key: constants.CONFIG_KEY_FONT_COLOR_TOP_MENU, value: '', readingRole: 'all'}),
+      ltiConsumersConfig: new Configuration({key: constants.CONFIG_KEY_LTI_CONSUMERS, value: '[]', readingRole: 'all'}),
     };
   },
   methods: {
@@ -133,6 +192,14 @@ export default {
         else if (this.topFontMenuColorConfig.value) {
           await this.topFontMenuColorConfig.save();
         }
+        if((!this.ltiConsumers || this.ltiConsumers.length===0) && this.ltiConsumersConfig.id!=null) {
+          await this.ltiConsumersConfig.delete();
+        }
+        else if(this.ltiConsumers || this.ltiConsumers.length>0){
+          this.ltiConsumersConfig.value = JSON.stringify(this.ltiConsumers);
+          await this.ltiConsumersConfig.save();
+        }
+
         this.$eventBus.$emit('configChanged', '');
         this.$notify({type: 'success', text: this.$t('notif-success-configuration-update')});
       }
@@ -174,6 +241,20 @@ export default {
       };
       this.openModal(props, events);
     },
+    confirmLTIConsumerDeletion(index) {
+      this.$buefy.dialog.confirm({
+        title: this.$t('confirm-deletion'),
+        message: this.$t('lti-confirm-deletion'),
+        type: 'is-danger',
+        confirmText: this.$t('button-confirm'),
+        cancelText: this.$t('button-cancel'),
+        onConfirm: () => {
+          {
+            this.ltiConsumers.splice(index, 1);
+          }
+        }
+      });
+    },
 
   },
   async created() {
@@ -189,6 +270,13 @@ export default {
     }
     catch(error) {
       // ignored
+    }
+    try {
+      await this.ltiConsumersConfig.fetch();
+      this.ltiConsumers = JSON.parse(this.ltiConsumersConfig.value);
+    }
+    catch(error) {
+      // no consumers definitions
     }
   }
 };
@@ -208,5 +296,30 @@ export default {
   margin-top: 0.75em;
   width: 2.25em;
 }
+button.edit {
+  height: 16px;
+  width: 16px;
+  background-color: rgba(10, 10, 10, 0.2);
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  pointer-events: auto;
+  display: inline-block;
+  font-size: 8px;
+  padding: 0;
+  outline: none;
+  position: relative;
+  vertical-align: top;
+  color: white;
+  margin-left: 0.5em;
+  line-height: 17px;
+  margin: 0;
+  margin-right: 0.25em;
+}
 
+.lti-consumer {
+  display: inline-block;
+  margin-top: 0em;
+  padding-right: 2px;
+}
 </style>
