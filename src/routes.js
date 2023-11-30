9dd3f3e53c08baf94374e2d4e9d6cc8b7cad3842
Mohamed Eltahir <mohamed.eltahir@cytomine.com>
2023-11-30 09:11:50 +0100

    feat: :sparkles: Adding appengine as the route + read task route via namespace & ver

--- src/routes.js
+++ src/routes.js
@@ -36,6 +36,7 @@ import ProjectHome from './components/project/ProjectHome.vue';
 import MemberActivityDetails from './components/project/activity/MemberActivityDetails.vue';
 import AdminPanel from './components/admin/AdminPanel.vue';
 import AppEngineMainPage from './components/appengine/AppEngineMainPage.vue';
+import AppInfo from './components/appengine/AppInfo.vue';
 import UserActivity from './components/user/UserActivity.vue';
 import PageNotFound from './components/PageNotFound.vue';
 import SoftwareInformation from './components/software/SoftwareInformation.vue';
@@ -146,8 +147,16 @@ const routes = [
   },
   // AppEngine 
   {
-    path: '/applications',
+    path: '/appengine',
     component: AppEngineMainPage
+  },
+  {
+    path: '/appengine/:id',
+    component: AppInfo,
+  },
+  {
+    path: '/appengine/:namespace/:version',
+    component: AppInfo,
   },
 
   // redirections for old URLS
