f2992d4b102ab497a8ff4df4fc22f40d44228e52
Mohamed Eltahir <mohamed.eltahir@cytomine.com>
2023-12-07 12:15:20 +0100

    refactor: :construction: AppEngineSidebar only houses it's content not responsible of ViewerSidebar job
    it won't do expanding, render an arrow ..etc stuff that ViewerSidebar should do

--- src/components/appengine/sidebar/AppEngineSidebar.vue
+++ src/components/appengine/sidebar/AppEngineSidebar.vue
@@ -1,73 +1,42 @@
 <template>
-  <div class="sidebar-wrapper" :class="{ expanded: expanded }" ref="ae_sidebar">
-    <nav class="sidebar" @click="clickHandler">
+  <div>
+    <nav class="sidebar">
       <h1 class="sidebar-title">{{ `${$t('app-engine')}: ${project.name}` }}</h1>
 
-      <section id="executor">
-        <a>
-          <i class="fas fa-cloud-upload-alt"></i>
-          {{ $t('ae-task-executor') }}
-        </a>
-      </section>
+      <div id="executor">
+        <application-executor />
+      </div>
 
-      <section id="status">
-        <a>
-          <i class="far fa-bell"></i>
-          {{ $t('ae-status') }}
-        </a>
-      </section>
+      <div id="status">
+        <application-current-status />
+      </div>
 
-      <section id="history">
-        <a>
-          <i class="far fa-file-alt"></i>
-          {{ $t('ae-history') }}
-        </a>
-      </section>
+      <div id="history">
+        <application-history />
+      </div>
 
     </nav>
-
-    <div class="arrow-sidebar" @click="expanded = false">
-      <i></i>
-    </div>
 
   </div>
 </template>
 
 <script>
 import { get, sync } from '@/utils/store-helpers';
-import constants from '@/utils/constants.js';
+import ApplicationExecutor from './ApplicationExecutor.vue';
+import ApplicationCurrentStatus from './ApplicationCurrentStatus.vue';
+import ApplicationHistory from './ApplicationsHistory.vue';
 
 export default {
   name: 'appengine-sidebar',
-  data() {
-    return {
-      algoEnabled: constants.ALGORITHMS_ENABLED
-    };
+  components: {
+    ApplicationExecutor,
+    ApplicationCurrentStatus,
+    ApplicationHistory
   },
   computed: {
     project: get('currentProject/project'),
     expanded: sync('currentUser/expandedSidebar')
   },
-  methods: {
-    clickHandler(event) {
-      let el = event.target;
-      // expand if user clicked on any part of the sidebar
-      if (!this.expanded) {
-        this.expanded = true;
-      }
-    },
-    transitionEndHandler() {
-      // led to a change of the size of the content div => need to reload OL map
-      // this to make sure that any one listing (CytomineImage.vue) will shrink the entire UI of what we see
-      this.$eventBus.$emit('updateMapSize');
-    }
-  },
-  mounted() {
-    this.$refs.ae_sidebar.addEventListener('transitionend', this.transitionEndHandler);
-  },
-  beforeDestroy() {
-    this.$refs.ae_sidebar.removeEventListener('transitionend', this.transitionEndHandler);
-  }
 };
 </script>
 
@@ -86,21 +55,8 @@ h1 {
   color: $color;
 }
 
-.sidebar-wrapper {
-  display: flex;
-  height: 100%;
-  background: $background;
-  overflow: hidden;
-  transition: width .2s linear;
-  width: 4rem;
-}
-
-.sidebar-wrapper.expanded {
-  width: 16rem;
-}
-
 .sidebar {
-  width: 16rem;
+  // width: 15rem;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -108,11 +64,11 @@ h1 {
 }
 
 .sidebar-title {
+  color: $activeColor;
   padding: 0.8em 0em 0.8em;
   width: 14.5rem !important;
   line-height: 1.8rem;
   text-align: center;
-  color: $background;
   font-size: 1rem;
   margin: 0;
   letter-spacing: 0.05em;
@@ -121,68 +77,9 @@ h1 {
   font-weight: 600;
 }
 
-.sidebar-wrapper.expanded .sidebar-title {
-  color: $activeColor !important;
-}
-
 section {
   width: 100%;
   height: 3.5rem;
-}
-
-.bottom-menu {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-}
-
-a {
-  position: relative;
-  display: block;
-  font-size: 0.85rem;
-  height: 100%;
-  font-weight: 600;
-  color: $color;
-  text-decoration: none;
-  display: flex;
-  align-items: center;
-}
-
-.fas,
-.far {
-  font-size: 1.25rem;
-  width: 4rem;
-  text-align: center;
-  flex-shrink: 0;
-}
-
-section a:hover {
-  color: $hoverColor;
-  background: $hoverBackground !important;
-}
-
-section.is-active a {
-  box-shadow: inset 5px 0 0 $hoverBackground;
-  color: $activeColor;
-  background: $activeBackground;
-}
-
-.arrow-sidebar {
-  width: 1.5rem;
-  border-left: 1px solid $border;
-  background: $arrowBackground;
-  justify-content: center;
-  display: flex;
-  align-items: center;
-}
-
-.arrow-sidebar i {
-  border: solid $arrowColor;
-  border-width: 0 2px 2px 0;
-  display: inline-block;
-  padding: 0.25rem;
-  transform: rotate(135deg);
-  margin-left: 0.25rem;
 }
 </style>
     
