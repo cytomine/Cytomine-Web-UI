78c92ef904543e6715f9fa47c2984e5cb8878723
Ba Thien LE <bathien.le.pro@gmail.com>
2025-05-09 09:53:44 +0200

    fix: overflow text over button

--- src/components/appengine/forms/fields/WsiField.vue
+++ src/components/appengine/forms/fields/WsiField.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <b-field label-position="on-border" expanded class="field">
       <template #label>
-        {{ parameter.display_name }}
+        <span class="text-label">
+          {{ parameter.display_name }}
+        </span>
       </template>
       <b-button @click="selectImage = true">
         {{ $t('select') }}
@@ -77,5 +79,14 @@ export default {
 .info {
   text-align: right;
   margin-left: 5px;
+}
+
+.text-label {
+  display: block;
+  width: 100%;
+  text-align: center;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 </style>
